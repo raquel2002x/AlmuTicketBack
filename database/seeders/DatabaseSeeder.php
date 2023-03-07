d141796cf3cca3c94d4b83fdaa52692d9327f3fb
Himorell <himorellj@gmail.com>
2023-03-07 10:30:48 +0100

    merge categoriesTable to dev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
        /*  User::factory(10)->create(); */
 
         User::factory()->create([
-             'name' => 'Coders',
-             'email' => 'coders@arrabalempleo.org',
-         ]);
+            'name' => 'Coders',
+            'email' => 'coders@arrabalempleo.org',
+        ]);
     }
 }
