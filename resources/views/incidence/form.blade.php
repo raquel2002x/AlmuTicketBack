5e541f0ebd10bcc6be7c63df2d213632ea1f1110
Himorell <himorellj@gmail.com>
2023-03-07 20:43:45 +0100

    reorder variables2

--- resources/views/incidence/form.blade.php
+++ resources/views/incidence/form.blade.php
@@ -5,32 +5,34 @@
         
         <div class="form-group">
             <label for="user">Usuario</label>
-            {!! Form::select('user_id', $users, null, ['class' => 'form-control']) !!}
+            {!! Form::select('user_id', $users, null, ['class' => 'form-control', 'name' => 'user_id']) !!}
         </div> 
         <div class="form-group">
             <label for="category">Categoría</label>
-            {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+            {!! Form::select('category_id', $categories, null, ['class' => 'form-control', 'name' => 'category_id']) !!}
         </div>
         <div class="form-group">
             <label for="area">Areas</label>
-            {!! Form::select('area_id', $areas, null, ['class' => 'form-control']) !!}
+            {!! Form::select('area_id', $areas, null, ['class' => 'form-control', 'name' => 'area_id']) !!}
         </div>
         <div class="form-group">
             <label for="location">Sede</label>
-            {!! Form::select('location_id', $locations, null, ['class' => 'form-control']) !!}
+            {!! Form::select('location_id', $locations, null, ['class' => 'form-control', 'name' => 'location_id']) !!}
         </div>
         <div class="form-group">
             <label for="state">Estado</label>
-            {!! Form::select('state_id', $states, null, ['class' => 'form-control']) !!}
+            {!! Form::select('state_id', $states, null, ['class' => 'form-control', 'name' => 'state_id']) !!}
         </div>
         <div class="form-group">
-            {{ Form::label('Descripcion') }}
-            {{ Form::text('description', $incidence->description, ['class' => 'form-control' . ($errors->has('description') ? ' is-invalid' : ''), 'placeholder' => 'Description']) }}
-            {!! $errors->first('description', '<div class="invalid-feedback">:message</div>') !!}
+            <label for="description">Descripción</label>
+            {{ Form::text('description', $incidence->description, ['class' => 'form-control' . ($errors->has('description') ? ' is-invalid' : ''), 'placeholder' => 'Descripcion del requerimiento']) }}
+            @if ($errors->has('description'))
+                <div class="invalid-feedback">{{ $errors->first('description') }}</div>
+            @endif
         </div>
 
     </div>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Enviar</button>
     </div>
 </div>
