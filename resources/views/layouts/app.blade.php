7fe0b7df08dcd11d44319c1af9944bc527468b54
Himorell <himorellj@gmail.com>
2023-03-09 10:39:11 +0100

    add link navbar2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,8 +56,8 @@
                                     <a class="nav-link" href="{{ route('bookings.index') }}">Bookings</a>
                                 </li><div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
